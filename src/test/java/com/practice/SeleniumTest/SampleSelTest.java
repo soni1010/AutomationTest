d7f0b41621d2bd48318f18b985a3d83911f2da34
hsoni2010@gmail.com <hsoni2010@gmail.com>
2020-02-09 12:14:37 +0530

    saple1

---
+++ src/test/java/com/practice/SeleniumTest/SampleSelTest.java
@@ -0,0 +1,50 @@
+package com.practice.SeleniumTest;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SampleSelTest {
+	
+	WebDriver driver;
+	SoftAssert softAssert = new SoftAssert();
+	
+	@Test
+	public void testMethod() throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver","D:/Sotwares/chromedriver79.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("http://harendersoni.blogspot.com/");
+		System.out.println("1 - "+driver.findElement(By.cssSelector("#header[name='Header']")).getText());
+		System.out.println("2 - "+driver.findElement(By.cssSelector(".title")).getText());
+		System.out.println("3 - "+driver.findElement(By.cssSelector("#write-the-code-you-want-to-see-as-comments-first")).getText());
+//		System.out.println("4 - "+driver.findElement(By.cssSelector(".^g-pr")).getText());
+		Thread.sleep(2000);
+		String s=driver.findElement(By.cssSelector("#header[name='Header']")).getText();
+		softAssert.assertEquals(s, "anything");
+		driver.quit();
+		
+		
+	}
+	
+	@Test
+	public void testMethod1() throws InterruptedException {
+
+		System.out.println("here");
+	}
+
+	@Test
+	public void A() throws InterruptedException {
+
+		System.out.println("here1");
+
+	}
+	
+}
